7802313c9677726109dd593b645d411e71216e9c
hemasatre <110393176+hemasatre@users.noreply.github.com>
2022-08-02 18:27:12 +0530

    By Hemlata

--- GitHub/src/aug1/Demo.java
+++ GitHub/src/aug1/Demo.java
@@ -6,6 +6,12 @@ public class Demo {
 	System.out.println("hub2");
 
 	}
+	public void m2()
+	{
+	System.out.println("New Method---");
+
+	}
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Git hub");
